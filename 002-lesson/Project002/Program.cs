51b9bf8dae58e3bf83e2ccdceacc357f8c23b6f3
Lennon_Ximenes <lennon_ximenes@hotmail.com>
2025-04-13 18:59:31 -0300

    Initial commit

---
+++ 002-lesson/Project002/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SecondProject {
+  internal class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+
+      byte x = 255;
+
+      Console.WriteLine(x);
+    }
+  }
+}
